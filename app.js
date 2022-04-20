378d56744e180fe16be9b59d3cd4fd3bcc289c48
Marija Strahinjic <marija.strahinjic@hotmail.com>
2022-04-20 12:47:52 +0200

    use handlebars to add html pages

--- app.js
+++ app.js
@@ -1,7 +1,15 @@
 const express = require('express');
 const app = express();
 
+app.set("views", __dirname + "/views");
+app.set("view engine", "hbs");
+
 app.use(express.static('public'));
+
+
+app.get("/", (req, res, next)=>{
+    res.sendFile(__dirname + '/views/home.html');
+});
 
 
 app.get("/about", (req, res, next) => {
@@ -11,6 +19,42 @@ app.get("/about", (req, res, next) => {
 
 app.get("/contact", (req, res, next) => {
     res.sendFile(__dirname + '/views/contact.html');
+});
+
+app.get("/rice", (req, res, next) => {
+
+    const data = {
+        title: "Rice",
+        price: 2,
+        imageFile: "rice.jpg",
+        stores: ["Online", "Albacete", "Freiburg", "Amsterdam"]
+    }
+
+    res.render("product", data);
+});
+
+app.get("/chicken", (req, res, next) => {
+    //res.sendFile(__dirname + '/views/product-chicken.html');
+
+    const data = {
+        title: "Chicken",
+        price: 5,
+        imageFile: "chicken.jpg"
+    }
+
+    res.render("product", data);
+});
+
+app.get("/seafood", (req, res, next) => {
+    //res.sendFile(__dirname + '/views/product-seafood.html');
+
+    const data = {
+        title: "Seafood",
+        price: 10,
+        imageFile: "seafood.jpg"
+    }
+
+    res.render("product", data);
 });
 
 
